fc1a042c8539998ab27a4c8a92075518ff01e0f0
kwebam <77285125+kwebam@users.noreply.github.com>
2021-05-24 09:56:29 +0530

    Add files via upload

---
+++ mian.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function setup() {
+          createCanvas(300 , 300);
+          canvasCenter()
+}
+function draw() {
+          
+}
+
+function takeSnapshot() {
+          save('myFilterImage.png');
+}
